4c76a10491b7e6c29d467697570ff7886f772be9
roopamgarg <roopamg777@gmail.com>
2020-02-29 11:29:45 +0530

    change deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+npm install
 npm run build
 tar czf pracify-user-ui.tar.gz package.json package-lock.json public build
 scp -i /home/roopam/.ssh/pracify_keys pracify-user-ui.tar.gz ubuntu@35.154.129.241:/var/www/
