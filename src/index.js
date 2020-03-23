7dc49b7d62465286433a2fab39372df51442325c
amitdtu <amitdtu007@gmail.com>
2020-03-23 11:28:32 +0530

    cookie.get

--- src/index.js
+++ src/index.js
@@ -7,13 +7,12 @@ import App from "./App";
 import "./index.css";
 import "./style/_style.scss";
 import "circular-std"; 
-import axios from 'axios';
-import Cookie from 'js-cookie';
+// import axios from 'axios';
+// import Cookie from 'js-cookie';
 
 // import * as serviceWorker from './serviceWorker';
 axios.defaults.baseURL = 'https://pracify.com/testing/';
-// axios.defaults.headers.common['token'] = Cookie.get('token');
-axios.defaults.headers.common['token'] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTZmNmM3NjM0MjJiNTZmODc3Mzg3OGMiLCJyZXN1bWUiOiI1ZTZmNmM3NjM0MjJiNTZmODc3Mzg3OGIiLCJpYXQiOjE1ODQzNjA1NjYsImV4cCI6MTU5MzAwMDU2Nn0.tbOs0uKlbj4cZ0-MGhoOWjqWqePNW842DuqdhuHsAvk";
+axios.defaults.headers.common['token'] = Cookie.get('token');
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
