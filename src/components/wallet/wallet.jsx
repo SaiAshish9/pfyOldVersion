e960f9185fe098ed6e48b2530573fdfa5bf32f1f
amitdtu <amitdtu007@gmail.com>
2020-03-25 22:49:52 +0530

    wallet new ui

--- src/components/wallet/wallet.jsx
+++ src/components/wallet/wallet.jsx
@@ -1,8 +1,11 @@
 import React,{useState, useEffect} from "react";
 import { Button, Icon, Tabs, message } from "antd";
-import coinIcon from "./coin.svg";
-import dateIcon from "./dateIcon.svg";
-import rupee1 from "./rupee1.svg";
+import coinIcon from "./images/coin.svg";
+import dateIcon from "./images/dateIcon.svg";
+import rupee1 from "./images/rupee1.svg";
+import envelop from './images/envelop.svg';
+import calander from './images/calander.svg';
+import future from './images/future.svg';
 // import rupee2 from "./rupee2.svg";
 import PaymentMethodModal from './paymentMethodModal';
 import axios from 'axios';
@@ -78,62 +81,80 @@ const Wallet = () => {
     console.log(key);
   }
 
+  const arr = [1,2,3]
   return (
-    <div className="wallet" style={{}}>
-      <PaymentMethodModal isModalOpen={isShow} isClose={isClose}/>
-      <div className="payment-summary">
-        <div className="summary-container">
-          <img src={coinIcon} alt="" className="money-image" />
-          <div className="content">
-            <h1 className="total-money">RS. {walletBalance}</h1>
-            <h4 className="message">Available Balance</h4>
-          </div>
-        </div>
+    // <div className="wallet" style={{}}>
+    //   {/* <PaymentMethodModal isModalOpen={isShow} isClose={isClose}/> */}
+    //   <div className="payment-summary">
+    //     <div className="summary-container">
+    //       <img src={coinIcon} alt="" className="money-image" />
+    //       <div className="content">
+    //         <h1 className="total-money">RS. {walletBalance}</h1>
+    //         <h4 className="message">Available Balance</h4>
+    //       </div>
+    //     </div>
 
-        <div className="button-container">
-          <Button onClick={isModalOpen} shape="round" className="add-payment">
-            ADD PAYMENT METHOD
-          </Button>
-          { walletBalance ? <Button onClick={redeem} shape="round" className="redeem-now">
-            REDEEM NOW
-          </Button> : null}
+    //     <div className="button-container">
+    //       <Button onClick={isModalOpen} shape="round" className="add-payment">
+    //         ADD PAYMENT METHOD
+    //       </Button>
+    //       { walletBalance ? <Button onClick={redeem} shape="round" className="redeem-now">
+    //         REDEEM NOW
+    //       </Button> : null}
+    //     </div>
+    //   </div>
+    //   <Tabs className="tab-options" defaultActiveKey="1" onChange={callback}>
+    //     <TabPane tab="Earnings" key="earnings">
+    //       {/* <Earnings data={earnings}/> */}
+    //     </TabPane>
+    //     <TabPane tab="Transactions" key="transactions">
+    //       {/* <Transactions data={transactions} /> */}
+    //     </TabPane>
+    //   </Tabs>
+    // </div>
+    <div className="wallet">
+      <div className="earning-block">
+        <div className="earning-inner-block">
+          <div className="payment-btn-and-earning">
+            <div className="coin-img">
+              <img src={coinIcon} alt=""/>
+            </div>
+            <div className="buttons">
+            <Button className="redeem-btn" shape={"round"}>Redeem Now</Button>
+            <Button className="add-payment-method-btn" shape={"round"}>Add Payment Method</Button>
+            </div>
+          </div>
+          <div className="earning-list-block">
+            <div className="title">Earnings</div>
+            <div className="list">
+              { arr.map(el => 
+                <div className="single-item">
+                <div className="envelop-img">
+                  <img src={envelop} alt=""/>
+                </div>
+                <div className="earning-description">
+                  <div>
+                    <div className="earning-description-title">Post on Instagram</div>
+                    <div className="earning-description-date"><img src={calander} alt=""/><span className="description-date-string">28  Dec 2020</span> </div>
+                  </div>
+                  <div>
+                    <div className="earning-description-amount">RS 50</div>
+                    <div className="earning-description-status"> <img src={future} alt=""/> <span className="description-status-string">Completed</span> </div>
+                  </div>
+                </div>
+              </div>
+                ) }
+            </div>
+          </div>
+          
         </div>
       </div>
-      <Tabs className="tab-options" defaultActiveKey="1" onChange={callback}>
-        <TabPane tab="Earnings" key="earnings">
-          <Earnings data={earnings}/>
-        </TabPane>
-        <TabPane tab="Transactions" key="transactions">
-          <Transactions data={transactions} />
-        </TabPane>
-      </Tabs>
-      {/* {a.map((element, i) => {
-        return (
-          <div className="transaction-detail" key={i}>
-            <div className="message">
-              <img src={rupee1} alt="" />
-              <h2 className="message-content">Amount paid</h2>
-            </div>
-            <div className="date-time-money">
-              <div className="date">
-                <img src={dateIcon} alt="" />
-                <h3 className="date-content">28 DEC 2019</h3>
-              </div>
-              <div className="time">
-                <img src={timeIcon} alt="" />
-                <h3 className="time-content">01:37 PM</h3>
-              </div>
-              <div className="rupee">
-                <img
-                  src="https://img.icons8.com/windows/32/000000/rupee.png"
-                  alt=""
-                ></img>
-                <h2 className="rupee-content">50</h2>
-              </div>
-            </div>
+      <div className="transaction-block">
+        <div className="transaction-inner-block">
+          <div>
           </div>
-        );
-      })} */}
+        </div>
+      </div>
     </div>
   );
 };
