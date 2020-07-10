a209f90bfbddbefc79993c8ec61da60cf5377748
mourrrya <mourrrya@gmail.com>
2020-07-11 04:32:45 +0530

    check

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import "./index.css";
 import "./style/_style.scss";
 axios.defaults.baseURL = "https://pracify.com/testing/";
 ReactDOM.render(<App />, document.getElementById("root"));
-
+ 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
